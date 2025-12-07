885bc264e3384ac8ae2a2041af35bc58d4581442
rajeshvcwork-dev <rajeshvc.work@gmail.com>
2025-12-07 16:38:51 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,10 +18,10 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white border-b border-border/50">
-      <div className="container mx-auto px-6 py-4">
+      <div className="container mx-auto px-6 py-8">
         <div className="flex items-center justify-between">
           <Link to="/" className="flex items-center gap-2">
-            <img src={vimicoLogo} alt="Vimico - You & I Co-operate" className="h-[80px] w-auto" />
+            <img src={vimicoLogo} alt="Vimico - You & I Co-operate" className="h-[90px] w-auto" />
           </Link>
 
           {/* Desktop Navigation */}
